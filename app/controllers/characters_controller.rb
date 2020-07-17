4748814334373b681ec1140c2f94d981ce3c46db
Reid Ashwill <reidashwill@gmail.com>
2020-07-17 09:18:26 -0700

    get skeleton of characters_controller and character model set up.

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,51 @@
+
+class CharactersController < ApplicationController
+  def index
+    @characters = Character.all
+    render :index
+  end
+
+  def new
+    @character = Character.new
+    render :new
+  end
+
+  def create
+    @character = Character.new(character_params)
+    if @character.save
+      redirect_to characters_path
+    else
+      render :new
+  end
+
+  def edit
+    @character = Character.find(params[:id])
+    render :edit
+  end
+
+  def show
+    @character = Character.find(params[:id])
+    render :show
+  end
+
+  def update
+    @character = Charated.find(params[:id])
+    if @character.update(character_params)
+      redirect_to characters_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @character = Character.find(params[:id])
+    @character.destroy
+    redirect_to character_path
+  end
+
+  private
+    def character_params
+      params.require(:character).permit(:name)
+    end
+
+end
